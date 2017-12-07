6483e796056699837ad23dd2ebbd47a5c1a327cb
KunQayz <p15232512@my365.dmu.ac.uk>
2017-12-07 10:28:49 +0000

    Final website
    Final changes made. Website Finished

--- js/javaScript.js
+++ js/javaScript.js
@@ -6,3 +6,35 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+var myIndex1 = 0;
+carousel1();
+
+function carousel1() {
+    var i;
+    var x = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex1++;
+    if (myIndex1 > x.length) {myIndex1 = 1}    
+    x[myIndex1-1].style.display = "block";  
+    setTimeout(carousel1, 4000); // Change image every 2 seconds
+}
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 4000); // Change image every 2 seconds
+}
+function myMessage() {
+    alert("Thank you for your submission!");
+}
